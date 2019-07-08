a8b2f0c6148e920655edd6a212c66d7aa3c6233a
GuoShiXin <3234655977@qq.com>
2019-07-08 17:16:37 +0800

    百度互联放入composer

--- src/GuoOauth.php
+++ src/GuoOauth.php
@@ -15,10 +15,73 @@ class GuoOauth
         $this->config = require_once('Config.php');
     }
 
-    public function testConnect()
+    //public function testConnect()
+    //{
+    //    return 'hello world!';
+    //}
+
+    public function baidu()
     {
-        return 'hello world!';
+        $clientId = $this->config['BAIDU']['BAIDU_API_KEY'];
+        $redirectUri = $this->config['BAIDU']['BAIDU_REDIRECT_URI'];
+        header("location:https://openapi.baidu.com/oauth/2.0/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&display=popup");
     }
 
+    public function getCode()
+    {
+        return $_GET['code'];
+    }
+
+    public function baiduAccessToken()
+    {
+        try{
+            $code = $this->getCode();
+            $redirect_uri  = $this->config['BAIDU']['BAIDU_REDIRECT_URI'];
+            $client_secret = $this->config['BAIDU']['BAIDU_SECRET_KEY'];
+            $client_id     = $this->config['BAIDU']['BAIDU_API_KEY'];
+            $url           = "https://openapi.baidu.com/oauth/2.0/token?grant_type=authorization_code&code=$code&client_id=$client_id&client_secret=$client_secret&redirect_uri=$redirect_uri";
+            $res           = $this->https_request($url);
+            $data          = json_decode($res, true);
+            return $data['access_token'];
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function baiduUserinfo()
+    {
+        try{
+            $accessToken = $this->baiduAccessToken();
+            $url  = 'https://openapi.baidu.com/rest/2.0/passport/users/getLoggedInUser?access_token='.$accessToken;
+            $res  = $this->https_request($url);
+            $userinfo = json_decode($res, true);
+            return $userinfo;
+        }catch (\Exception $e){
+            return $e->getMessage();
+        }
+    }
+
+
+    public static function https_request($url, $data = null,$ua=null)
+    {
+        # 初始化一个cURL会话
+        $curl = curl_init();
+        //设置请求选项, 包括具体的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);  //禁用后cURL将终止从服务端进行验证
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)){
+            curl_setopt($curl, CURLOPT_POST, 1);  //设置为post请求类型
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);  //设置具体的post数据
+        }
+        if($ua){
+            $useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11";
+            curl_setopt($curl, CURLOPT_USERAGENT, $useragent);        //模拟常用浏览器的useragent
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl);  //执行一个cURL会话并且获取相关回复
+        curl_close($curl);  //释放cURL句柄,关闭一个cURL会话
+        return $response;
+    }
 
 }
